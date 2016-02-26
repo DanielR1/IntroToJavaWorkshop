7bcd364aee8f8ab409442584c0780016ad35241f
DanielR1 <league@Mac-mini.local>
2016-02-25 18:30:40 -0800

    f
    f

--- src/day3/SpeakAndSpell.java
+++ src/day3/SpeakAndSpell.java
@@ -15,13 +15,24 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-		speak("phoenix");
+		speak("Czechoslovakia ");
 
 		// 2. Catch the user's answer in a String
+		String answer = JOptionPane.showInputDialog("How do you spell that word?");
 
 		// 3. If the user spelled the word correctly, speak "correct"
+				if(answer.equalsIgnoreCase("czachoslovakia"))
+				{
+					speak("that is correct");
+					
+				}
 
 		// 4. Otherwise say "wrong"
+				else
+				{
+					speak("that is incorrect, try again");
+					
+				}
 
 		// 5. repeat the process for other words
 		
