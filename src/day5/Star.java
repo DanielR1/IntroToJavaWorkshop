a8469ed4fc80cc98176bd10490e6e7c1a0f9e734
DanielR1 <league@Mac-mini.local>
2016-03-16 18:57:09 -0700

    f
    f

---
+++ src/day5/Star.java
@@ -0,0 +1,30 @@
+package day5;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Star {
+	public static void main(String[] args) {
+		Robot star = new Robot();
+		star.setSpeed(10);
+		star.turn(-90);
+		star.move(600);
+		star.turn(90);
+		star.turn(18);
+		for (int j = 0; j < 4; j++) {
+			if(j<0){
+			
+			}
+		
+		for (int i = 0; i < 8; i++) {
+			star.penDown();
+			
+			star.move(300);
+			star.turn(144);
+	
+			
+			
+		}
+	}
+	}
+
+}
